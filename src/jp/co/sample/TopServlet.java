06d1fc279c3876cd8017947841528c0e473aed86
Shinji Miura <40balmung@gmail.com>
2020-06-24 16:20:44 +0900

    first commit

---
+++ src/jp/co/sample/TopServlet.java
@@ -0,0 +1,28 @@
+package jp.co.sample;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TopServlet
+ */
+@WebServlet("/index")
+public class TopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/index.jsp");
+		rd.forward(request, response);
+	}
+
+}
